e354b1d2fe7bfad1e9149fbd42716535f85c82fa
Noble777 <guolei1.chen@gmail.com>
2018-11-29 20:59:52 -0500

    update

--- week5-codelab/web_server/server/app.js
+++ week5-codelab/web_server/server/app.js
@@ -1,7 +1,9 @@
+var auth = require('./routes/auth');
+var authChecker = require('./auth/auth_checker');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var express = require('express');
-var path = require('path');
-
-var express = require('express');
+var passport = require('passport');
 var path = require('path');
 
 var index = require('./routes/index');
@@ -9,19 +11,28 @@ var news = require('./routes/news');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+var config = require('./config/config.json');
+
+require('./models/main.js').connect(config.mongoDbUri);
+
+// load passport strategies.
+app.use(passport.initialize());
+passport.use('local-signup', require('./auth/signup_local_strategy'));
+passport.use('local-login', require('./auth/login_local_strategy'));
+
 // view engine setup
 app.set('views', path.join(__dirname, '../client/build'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 
 // TODO: remove this after development is done
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
 
 app.use('/', index);
+app.use('/auth', auth);
+app.use('/news', authChecker);
 app.use('/news', news);
 
 // catch 404 and forward to error handler
