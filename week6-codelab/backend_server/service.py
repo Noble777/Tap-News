284fa2bb072bd08da9554b4d4e2ae510e6101773
Noble777 <guolei1.chen@gmail.com>
2018-12-05 21:59:57 -0500

    make a codelab for client and server pagination

---
+++ week6-codelab/backend_server/service.py
@@ -0,0 +1,35 @@
+""" Backend service """
+import logging
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+import operations
+
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 4040
+
+LOGGER_FORMAT = '%(asctime)s - %(message)s'
+logging.basicConfig(format=LOGGER_FORMAT)
+LOGGER = logging.getLogger('backend_service')
+LOGGER.setLevel(logging.DEBUG)
+
+
+def add(num1, num2):
+    """ Test method """
+    LOGGER.debug("add is called with %d and %d", num1, num2)
+    return num1 + num2
+
+
+def get_one_news():
+    """ Test method to get one news """
+    LOGGER.debug("getOneNews is called")
+    return operations.getOneNews()
+
+
+# Threading RPC Server
+RPC_SERVER = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
+RPC_SERVER.register_function(add, 'add')
+RPC_SERVER.register_function(get_one_news, 'getOneNews')
+
+LOGGER.info("Starting RPC server on %s:%d", SERVER_HOST, SERVER_PORT)
+
+RPC_SERVER.serve_forever()
