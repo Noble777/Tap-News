b27eae3cfbcc7dab72846bec88a47d4b9f38411a
Noble777 <guolei1.chen@gmail.com>
2018-12-10 13:18:50 -0500

    make a copy for deploy ml

---
+++ week7-demo/web_server/client/src/index.js
@@ -0,0 +1,8 @@
+import Base from './Base/Base';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Base />, document.getElementById('root'));
+registerServiceWorker();
+
